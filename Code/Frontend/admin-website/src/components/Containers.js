7a1049cd56ba64db7657c2661051f0feaab15ac5
Matthias Greeven <matthidg@stud.ntnu.no>
2023-02-23 17:33:39 +0100

    removed unncessary import

--- Code/Frontend/admin-website/src/components/Containers.js
+++ Code/Frontend/admin-website/src/components/Containers.js
@@ -1,9 +1,7 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
 import './Containers.css';
 
 export default function Containers() {
-    let navigate = useNavigate();
   return (
     <div className='inventory'>
         Inventory page 
