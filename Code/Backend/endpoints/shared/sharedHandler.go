ad2b9b0779999fed0f5900b3c5e2da62cfdd8ec6
unknown <lars.l.r@hotmail.com>
2023-03-16 06:47:53 +0100

    [#66] Add POST to Container endpoint.

--- Code/Backend/endpoints/shared/sharedHandler.go
+++ Code/Backend/endpoints/shared/sharedHandler.go
@@ -173,10 +173,10 @@ func HandlerContainer(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Split the path on each "/", unless the path is blank
-	args := []string{}
+	/* args := []string{}
 	if len(escapedPath) > 0 {
 		args = strings.Split(escapedPath, "/")
-	}
+	} */
 
 	// Switch based on method
 	switch r.Method {
@@ -205,9 +205,81 @@ func HandlerContainer(w http.ResponseWriter, r *http.Request) {
 
 	// POST method
 	case http.MethodPost:
-		// If there's not enough args, return
-		if len(args) < 1 {
-			http.Error(w, "Not enough arguments, read the documentation for more information.", http.StatusUnprocessableEntity)
+		// Decode body
+		var data []map[string]interface{}
+		err := json.NewDecoder(r.Body).Decode(&data)
+		if err != nil {
+			http.Error(w, "Could not decode body.", http.StatusUnprocessableEntity)
+			return
+		}
+
+		var propsQuery strings.Builder
+		i := 0
+
+		// Reorder into format: ["propertyName":[value1, value2, value3]]
+		// (And assembly prop query)
+		props_values := make(map[string]([]interface{}))
+		var props []string
+		for _, kvp := range data {
+			for k, v := range kvp {
+
+				// Check if prop already exists in props
+				propExists := false
+				for _, prop := range props {
+					if prop == k {
+						propExists = true
+						break
+					}
+				}
+				if !propExists {
+					props = append(props, k)
+					if i > 0 {
+						propsQuery.WriteString(", ")
+					}
+					propsQuery.WriteString(fmt.Sprintf("`%s`", k))
+					i++
+				}
+
+				props_values[k] = append(props_values[k], v)
+			}
+		}
+
+		// Assemble values string
+		var valuesQuery strings.Builder
+		for i, kvp := range data {
+			if i > 0 {
+				valuesQuery.WriteString("), (")
+			}
+			for j, prop := range props {
+				if j > 0 {
+					valuesQuery.WriteString(", ")
+				}
+				v := kvp[prop]
+				if v == nil {
+					valuesQuery.WriteString("NULL")
+					continue
+				}
+				valuesQuery.WriteString(fmt.Sprintf("'%v'", v))
+			}
+		}
+
+		// Assemble final query and query it
+		query := fmt.Sprintf("INSERT INTO `container` (%s) VALUES (%s)", propsQuery.String(), valuesQuery.String())
+		fmt.Println("query: ", query)
+		var args []interface{}
+		res, err := globals.QueryJSON(globals.DB, query, args, w)
+		if err != nil {
+
+			fmt.Println(err)
+			http.Error(w, "Error fetching containers.", http.StatusInternalServerError)
+			return
+		}
+
+		// Set header and encode to writer
+		w.Header().Set("Content-Type", "application/json")
+		err = json.NewEncoder(w).Encode(res)
+		if err != nil {
+			http.Error(w, "Error encoding containers.", http.StatusInternalServerError)
 			return
 		}
 
