b182c5308406c8aeba179c065e2f68c75f37474d
haavbo <haavbo@stud.ntnu.no>
2023-03-23 12:51:42 +0100

    Minor changes to main fragment.

--- Code/Frontend/Mobile application/Logistics/app/src/main/java/cryogenetics/logistics/ui/sidebar/SidebarFragment.kt
+++ Code/Frontend/Mobile application/Logistics/app/src/main/java/cryogenetics/logistics/ui/sidebar/SidebarFragment.kt
@@ -20,7 +20,7 @@ class SidebarFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_sidebar, container, false)
+        return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
