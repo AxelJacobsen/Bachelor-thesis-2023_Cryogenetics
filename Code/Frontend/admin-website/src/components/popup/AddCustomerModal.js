5f0115a74febe7d5b4e9d7411683950433428998
Matthias Greeven <matthidg@stud.ntnu.no>
2023-04-18 18:21:07 +0200

    remove import

--- Code/Frontend/admin-website/src/components/popup/AddCustomerModal.js
+++ Code/Frontend/admin-website/src/components/popup/AddCustomerModal.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
-import { Button, TextField, MenuItem } from '@mui/material';
+import { Button, TextField } from '@mui/material';
 import fetchData from '../../globals/fetchData';
 
 const style = {
